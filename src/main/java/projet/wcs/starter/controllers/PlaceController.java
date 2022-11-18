1b386049fce0736fe2fa3f281981c90f3aba823c
matgoy <gohiermathieu78@gmail.com>
2022-11-18 14:16:13 +0100

    place filter by id

--- src/main/java/projet/wcs/starter/controllers/PlaceController.java
+++ src/main/java/projet/wcs/starter/controllers/PlaceController.java
@@ -1,12 +1,22 @@
 package projet.wcs.starter.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import projet.wcs.starter.entities.Place;
 import projet.wcs.starter.repositories.PlaceRepository;
 
+import java.util.Optional;
 
+@RestController
+@CrossOrigin
 public class PlaceController {
 
     @Autowired
     private PlaceRepository placeRepository;
+
+    @RequestMapping("/places/{id}")
+    public Optional<Place> getPlaces(@PathVariable long id){
+        return placeRepository.findById(id);
+    }
 
 
 
--- src/main/java/projet/wcs/starter/repositories/PlaceRepository.java
+++ src/main/java/projet/wcs/starter/repositories/PlaceRepository.java
@@ -3,7 +3,12 @@ package projet.wcs.starter.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import projet.wcs.starter.entities.Place;
+import projet.wcs.starter.entities.Room;
+
+import java.util.List;
 
 @Repository
 public interface PlaceRepository extends JpaRepository<Place,Long> {
+
+
 }
